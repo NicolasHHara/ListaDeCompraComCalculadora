b63e2253d53db71e8c5cfc7cb12b5c10bb0688f2
Nicolas <nico.hhara@gmail.com>
2025-04-05 21:18:49 -0300

    .

---
+++ script.js
@@ -0,0 +1,23 @@
+// Variáveis de referência
+let btnComprar = document.getElementById("btnComprar");
+let cestaContainer = document.getElementById("container2");  // Onde os itens do carrinho serão exibidos
+let cesta = [];
+
+// Função chamada quando o botão "Comprar" é clicado
+function addCesta() {
+    let produto1 = { 
+        nome: "Cassiano",
+        imagem: "/ListaDeCompraComCalculadora/im1.jpeg",            //array
+        preco: "1,99"
+};
+    console.log (produto1);       
+}
+
+function addCesta() {
+    let produto2 = { 
+        nome: "Dudu",
+        imagem: "/ListaDeCompraComCalculadora/img2.jpeg",            //array
+        preco: "30"
+    };
+    console.log (produto2)
+}
