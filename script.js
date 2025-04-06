9afbfeae9b3e67a1ab06adbc58e7c635369c4127
Nicolas <nico.hhara@gmail.com>
2025-04-06 00:33:28 -0300

    javaScript
    object

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
 // Variáveis de referência
-let btnComprar = document.getElementById("btnComprar");
+let btnComprar1 = document.getElementById("btnComprar1");
+let btnComprar2 = document.getElementById("btnComprar2");
 let cestaContainer = document.getElementById("container2");  // Onde os itens do carrinho serão exibidos
 let cesta = [];
 
-// Função chamada quando o botão "Comprar" é clicado
-function addCesta() {
-    let produto1 = { 
-        nome: "Cassiano",
-        imagem: "/ListaDeCompraComCalculadora/im1.jpeg",            //array
-        preco: "1,99"
-};
-    console.log (produto1);       
+let produto1 = {
+    nome: "Cassiano",
+    imagem: "/ListaDeCompraComCalculadora/im1.jpeg",            //object
+    preco: "1,99"
+} 
+let produto2 = { 
+    nome: "Dudu",
+    imagem: "/ListaDeCompraComCalculadora/img2.jpeg",            //object
+    preco: "30"
 }
 
-function addCesta() {
-    let produto2 = { 
-        nome: "Dudu",
-        imagem: "/ListaDeCompraComCalculadora/img2.jpeg",            //array
-        preco: "30"
-    };
+
+function addCesta(produto) {
+    addCesta = btnComprar1;
+    cesta.push(produto);
+    atualizarCesta1();
+}
+function atualizarCesta1 (){
+    console.log (produto2)
+}
+
+
+function addCesta(produto) {
+    cesta.push(produto);
+    atualizarCesta2();
+}
+
+function atualizarCesta2 (){
     console.log (produto2)
 }
